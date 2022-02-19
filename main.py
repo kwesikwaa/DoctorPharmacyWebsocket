e0fd294247325e2925384a7aa8fbfdd0342775a7
kwesikwaa <kwesikwaahayford@gmail.com>
2022-02-19 17:07:53 +0000

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+@app.websocket('/hospital')
+async def connectsocket(skt: WebSocket):
+    print('Connecting.....')
+    await skt.accept()
+    print('....connected')
+    while True:
+        try:
+            text = await skt.receive_text()
+            await skt.send_text(text)
+        except:
+            break
